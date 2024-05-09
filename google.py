fba850b0d5ecaae0aa0cacd5d6f3333d50a55c6e
Ritesen Dhar <101635863+ritesend@users.noreply.github.com>
2024-05-09 08:48:17 +0530

    Create google.py

---
+++ google.py
@@ -0,0 +1,22 @@
+gcloud auth list
+
+cloudshell edit file_name.py
+
+import flask
+
+# If entrypoint is not defined in app.yaml, App Engine will look for an app
+# called app in main.py.
+app = flask.Flask(_name_)
+
+
+@app.get("/")
+def hello():
+    """Return a friendly HTTP greeting."""
+    return "Hello World!\n"
+
+
+if _name_ == "_main_":
+    # Used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an entrypoint to app.yaml.
+    app.run(host="localhost", port=8080, debug=True)
